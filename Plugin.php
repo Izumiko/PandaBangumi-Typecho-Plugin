dc1d15065b7315d4e47c1f506d38a18fb9ed4107
Izumiko <yosoro@outlook.com>
2023-12-28 02:55:16 +0800

    更新Typecho 1.2

--- Plugin.php
+++ Plugin.php
@@ -1,4 +1,20 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+<?php
+
+namespace TypechoPlugin\PandaBangumi;
+
+use Typecho\Plugin\PluginInterface;
+use Typecho\Plugin\Exception as PluginException;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Radio;
+use Typecho\Widget\Helper\Form\Element\Checkbox;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Widget\Options;
+use Utils\Helper;
+
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+    exit;
+}
+
 /**
  * 给博客添加精美的番剧展示页吧！
  *  
@@ -11,27 +27,27 @@
 
 define('PandaBangumi_Plugin_VERSION', '2.3');
 
-class PandaBangumi_Plugin implements Typecho_Plugin_Interface
+class Plugin implements PluginInterface
 {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
      * 
      * @access public
      * @return void
-     * @throws Typecho_Plugin_Exception
+     * @throws PluginException
      */
-    public static function activate()
+    public static function activate(): void
     {
         // 检查是否存在对应扩展
         if (!extension_loaded('openssl')) {
-            throw new Typecho_Plugin_Exception('启用失败，PHP 需启用 OpenSSL 扩展。');
+            throw new PluginException('启用失败，PHP 需启用 OpenSSL 扩展。');
         }
         if (!extension_loaded('curl')) {
-            throw new Typecho_Plugin_Exception('启用失败，PHP 需启用 CURL 扩展。');
+            throw new PluginException('启用失败，PHP 需启用 CURL 扩展。');
         }
 
-        Typecho_Plugin::factory('Widget_Archive')->header = array('PandaBangumi_Plugin', 'header');
-        Typecho_Plugin::factory('Widget_Archive')->footer = array('PandaBangumi_Plugin', 'footer');
+        \Typecho\Plugin::factory('Widget_Archive')->header = __CLASS__ . '::header';
+        \Typecho\Plugin::factory('Widget_Archive')->footer = __CLASS__ . '::footer';
         Helper::addRoute("route_PandaBangumi","/PandaBangumi","PandaBangumi_Action",'action');
     }
 
@@ -41,9 +57,8 @@ class PandaBangumi_Plugin implements Typecho_Plugin_Interface
      * @static
      * @access public
      * @return void
-     * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate()
+    public static function deactivate(): void
     {
         Helper::removeRoute("route_PandaBangumi");
     }
@@ -52,38 +67,39 @@ class PandaBangumi_Plugin implements Typecho_Plugin_Interface
      * 获取插件配置面板
      * 
      * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @param Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form){
+    public static function config(Form $form): void
+    {
         echo '作者：<a href="https://www.imalan.cn">熊猫小A</a>，插件介绍页：<a href="https://blog.imalan.cn/archives/128/">熊猫追番 (PandaBangumi) for Typecho</a><br>';
         echo '<br><strong>使用方法，在文章要插入的地方写：</strong><br>';
         echo htmlspecialchars('所有在看：<div data-type="watching" class="bgm-collection"></div>'); echo '<br>';
         echo htmlspecialchars('已看动画：<div data-type="watched" data-cate="anime" class="bgm-collection"></div>'); echo '<br>';
         echo htmlspecialchars('已看三次元：<div data-type="watched" data-cate="real" class="bgm-collection"></div>'); echo '<br>';
 
-        $ID = new Typecho_Widget_Helper_Form_Element_Text('ID', NULL, '', _t('用户 ID'), _t('填写你的 Bangumi 主页链接 user 后面那一串数字'));
+        $ID = new Text('ID', NULL, '', _t('用户 ID'), _t('填写你的 Bangumi 主页链接 user 后面那一串数字'));
         $form->addInput($ID);
 
-        $PageSize = new Typecho_Widget_Helper_Form_Element_Text('PageSize', NULL, '6', _t('每页数量'), _t('填写番剧列表每页数量，填写 -1 则在一页内全部显示，默认为 6.'));
+        $PageSize = new Text('PageSize', NULL, '6', _t('每页数量'), _t('填写番剧列表每页数量，填写 -1 则在一页内全部显示，默认为 6.'));
         $form->addInput($PageSize);
 
-        $ValidTimeSpan = new Typecho_Widget_Helper_Form_Element_Text('ValidTimeSpan', NULL, '86400', _t('缓存过期时间'), _t('设置缓存过期时间，单位为秒，默认 24 小时。'));
+        $ValidTimeSpan = new Text('ValidTimeSpan', NULL, '86400', _t('缓存过期时间'), _t('设置缓存过期时间，单位为秒，默认 24 小时。'));
         $form->addInput($ValidTimeSpan);
 
-        $bgmst= new Typecho_Widget_Helper_Form_Element_Checkbox(
+        $bgmst= new Checkbox(
             'bgmst',  
             array('jq'=>_t('配置是否引入 JQuery：勾选则引入不勾选则不引入')),
             array('jq'), _t('基本设置'));
         $form->addInput($bgmst);
 
-        $ParseMethod = new Typecho_Widget_Helper_Form_Element_Radio('ParseMethod', array(
+        $ParseMethod = new Radio('ParseMethod', array(
             'api' => 'API',
             'webpage' => '网页'), 'api', 
             '已看列表解析方式', 'API 解析相对稳定，但是有最多获取最近 25 部的限制。网页解析速度可能较慢，但能获取更多记录。不影响在看列表。');
         $form->addInput($ParseMethod);
 
-        $Limit = new Typecho_Widget_Helper_Form_Element_Text('Limit', NULL, '20', _t('已看列表数量限制'), _t('设置获取数量限制，不建议设置得太大，有被 Bangumi 拉黑的风险。<b>仅当通过网页解析时有效</b>。不影响在看列表。'));
+        $Limit = new Text('Limit', NULL, '20', _t('已看列表数量限制'), _t('设置获取数量限制，不建议设置得太大，有被 Bangumi 拉黑的风险。<b>仅当通过网页解析时有效</b>。不影响在看列表。'));
         $form->addInput($Limit);
     }
 
@@ -91,44 +107,47 @@ class PandaBangumi_Plugin implements Typecho_Plugin_Interface
      * 个人用户的配置面板
      * 
      * @access public
-     * @param Typecho_Widget_Helper_Form $form
+     * @param Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
-    
+    public static function personalConfig(Form $form){}
+
     /**
      * 输出头部css
-     * 
+     *
      * @access public
      * @return void
+     * @throws PluginException
      */
-    public static function header()
+    public static function header(): void
     {
+        $config = Options::alloc()->plugin('PandaBangumi');
+
+
         echo '<link rel="stylesheet" href="';
-        Helper::options()->pluginUrl('/PandaBangumi/css/PandaBangumi.25.css');
+        Options::alloc()->pluginUrl('/PandaBangumi/css/PandaBangumi.25.css');
         echo '?v='.PandaBangumi_Plugin_VERSION.'" />';
-        if (!empty(Helper::options()->plugin('PandaBangumi')->bgmst) && in_array('jq', Helper::options()->plugin('PandaBangumi')->bgmst))
+        if (!empty($config->bgmst) && in_array('jq', $config->bgmst))
         {
             echo '<script src="';
-            Helper::options()->pluginUrl('/PandaBangumi/js/jq.min.js');
+            Options::alloc()->pluginUrl('/PandaBangumi/js/jq.min.js');
             echo '"></script>';
         }
-        echo '<script>var bgmBase="';
-        Helper::options()->index('/PandaBangumi');
+        echo '<script>const bgmBase="';
+        Options::alloc()->index('/PandaBangumi');
         echo '"</script>';
-    }  
+    }
 
     /**
      * 在底部输出所需 JS
-     * 
+     *
      * @access public
-     * @param Typecho_Widget_Helper_Form $form
      * @return void
      */
-    public static function footer()
+    public static function footer(): void
     {
         echo '<script type="text/javascript" src="';
-        Helper::options()->pluginUrl('/PandaBangumi/js/PandaBangumi.24.js');
+        Options::alloc()->pluginUrl('/PandaBangumi/js/PandaBangumi.24.js');
         echo '?v='.PandaBangumi_Plugin_VERSION.'"></script>';
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 # PandaBangumi-Typecho-Plugin
 
-![](https://cdn.imalan.cn/img/site/IMG_1676.JPG)
-
 为你的 Typecho 博客增加追番列表显示功能。
 
-演示：**[我的追番 - 熊猫小A的博客](https://blog.imalan.cn/bangumi/)**
-
 介绍：**[熊猫追番 (PandaBangumi) for Typecho 发布！ - 熊猫小A的博客](https://blog.imalan.cn/archives/128/)**
+
+## 使用
+
+插件版添加了**分页功能**，这样追番很多时能节约流量，加快速度。追番列表与追番日历功能都可以自己选择要不要开启，在插件里设置就好。
+
+使用方法：去 GitHub 上下载插件：[Izumiko/PandaBangumi-Typecho-Plugin](https://github.com/Izumiko/PandaBangumi-Typecho-Plugin)
+
+解压后把文件夹改名为 `PandaBangumi` ，上传到服务器 `usr/plugins` 目录下，在 Typecho 后台启用本插件，填写 ID（即用户主页链接后的那串数字），设置一下每页展示的数量。
+
+在任何页面，不论是独立页还是一般的文章页面，在文章里插入代码：
+
+在看
+
+```html
+<div data-type="watching" class="bgm-collection"></div>
+```
+
+动漫已看
+
+```html
+<div data-type="watched" data-cate="anime" class="bgm-collection"></div>
+```
+
+三次元已看
+
+```html
+<div data-type="watched" data-cate="real" class="bgm-collection"></div>
+```
+
+保存发布，这个位置就会展开成追番展示面板。加载和分页都使用 AJAX 请求～
+
+插件带了缓存功能，可以极大地提升速度，**但是记得要保证 `插件目录/json/` 这个目录可写**。
+
+## 注意事项
+
+服务器需要启用 PHP curl openssl 扩展。
+
+不一定所有主题都完美。
+
+模板会向 `插件目录/json/` 写入缓存数据，请保证这个目录可写。
+
+**如果你发现 PJAX 切换页面时番剧都不加载了，你需要去主题的 PJAX 回调函数中添加一句 initCollection();**
